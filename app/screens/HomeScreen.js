15b448ed979955b9dc5eecb298fed2febb869748
fabien0102 <fabien0102@planee.fr>
2017-10-05 10:39:09 +0200

    Add a welcoming message

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { getUserQuery } from "./LoginScreen";
+import { graphql } from "react-apollo";
 
 /**
  * Home screen
@@ -9,16 +11,20 @@ import { View, Text } from "react-native";
  *  - routes tries but not finished
  *  - max grade reach
  */
-export default class HomeScreen extends React.Component {
+export class HomeScreen extends React.Component {
   static navigationOptions = {
     title: "Home"
   };
 
   render() {
+    console.log(this.props);
     return (
       <View>
         <Text>HomeScreen</Text>
+        <Text>Welcome {this.props.me.user.pseudo}</Text>
       </View>
     );
   }
 }
+
+export default graphql(getUserQuery, { name: "me" })(HomeScreen);
