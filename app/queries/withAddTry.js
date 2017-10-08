054dd3ebc0f038aca9a34689ae26494ddd48b856
fabien0102 <fabien0102@planee.fr>
2017-10-08 12:00:53 +0200

    Export updateRouteDetailQuery

--- app/queries/withAddTry.js
+++ app/queries/withAddTry.js
@@ -3,27 +3,29 @@ import addTryMutation from "./addTryMutation.graphql";
 import routeDetailQuery from "./routeDetailQuery.graphql";
 import { omit } from "lodash";
 
+const updateRouteDetailQuery = (proxy, id, createTry) => {
+  // Get cached data
+  let routeDetailData = proxy.readQuery({
+    query: routeDetailQuery,
+    variables: { id }
+  });
+  // Mutate with received data
+  if (createTry.route.averageTries >= 0) {
+    Object.assign(routeDetailData.Route, createTry.route);
+  }
+  routeDetailData.Route.tries.unshift(omit(createTry, "route"));
+  // Update cache
+  proxy.writeQuery({
+    query: routeDetailQuery,
+    variables: { id },
+    data: routeDetailData
+  });
+};
+
 export default graphql(addTryMutation, {
   options: ({ navigation: { state: { params: { id } } } }) => ({
     update: (proxy, { data: { createTry } }) => {
-      // Get cached data
-      let routeDetailData = proxy.readQuery({
-        query: routeDetailQuery,
-        variables: { id }
-      });
-
-      // Mutate with received data
-      if (createTry.route.averageTries >= 0) {
-        Object.assign(routeDetailData.Route, createTry.route);
-      }
-      routeDetailData.Route.tries.unshift(omit(createTry, "route"));
-
-      // Update cache
-      proxy.writeQuery({
-        query: routeDetailQuery,
-        variables: { id },
-        data: routeDetailData
-      });
+      updateRouteDetailQuery(proxy, id, createTry);
 
       // TODO update route if success level is 5
     }
