577c29e7d9ade2aee97e8dc71670db6d036d38aa
fabien0102 <fabien0102@planee.fr>
2017-10-11 12:47:01 +0200

    Add test structure

---
+++ app/queries/withTriesStats.test.js
@@ -0,0 +1,17 @@
+import {
+  getAfterWorkCount,
+  getFlashCount,
+  getNotFinishedCount,
+  getSuccessRates
+} from "./withTriesStats";
+
+const {
+  data: { allTries }
+} = require("./__tests__/allTriesQueryResponse.json");
+
+describe("withTriesStats", () => {
+  describe("getSuccessRates", () => {});
+  describe("getFlashCount", () => {});
+  describe("getAfterWorkCount", () => {});
+  describe("getNotFinishedCount", () => {});
+});
