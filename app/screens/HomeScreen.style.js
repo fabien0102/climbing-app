e97e44d3b90c61b54c4d5662ee7c1e257bb13ddd
fabien0102 <fabien0102@planee.fr>
2017-10-16 14:51:28 +0200

    Add basic style & data on HomeScreen

---
+++ app/screens/HomeScreen.style.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  main: {
+    backgroundColor: "#f9f9f9"
+  },
+  avatar: {
+    backgroundColor: "#00ACBF",
+    paddingTop: 30,
+    paddingBottom: 15,
+    height: 160,
+    alignItems: "center",
+    marginBottom: 20
+  },
+  pseudo: {
+    fontSize: 22,
+    color: "white"
+  },
+  progressBarPart: {
+    paddingHorizontal: 20,
+    marginTop: 20
+  },
+  progressBarLabel: {
+    fontSize: 15,
+    color: "gray"
+  },
+  progressBarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10
+  },
+  progressBar: {
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    borderRadius: 12,
+    height: 12,
+    overflow: "hidden"
+  },
+  progressBarItem: {
+    flexShrink: 0,
+    flexGrow: 0
+  },
+  progressBarValue: {
+    fontSize: 10,
+    color: "grey",
+    marginHorizontal: 5
+  },
+  gradeStat: {
+    alignItems: "center"
+  },
+  gradeStatValue: {
+    color: "#666666"
+  }
+});
